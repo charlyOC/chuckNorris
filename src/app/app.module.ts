8612d9b97523e391c92a81c2dffcffd1d17cd64e
Guillaumelsn <guillaumelesenne@gmail.com>
2022-03-14 16:18:15 +0100

    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MainPageComponent } from './mainPage/main-page-component';
-
 import { AppComponent } from './app.component';
 import { JokePageComponent } from './joke-page/joke-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { JokePageComponent } from './joke-page/joke-page.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
