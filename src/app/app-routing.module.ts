94e13f04d4a24390c8d29cc52cef1b9db60e92ae
Guillaumelsn <guillaumelesenne@gmail.com>
2022-03-14 16:02:14 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { MainPageComponent } from './mainPage/main-page-component';
+import { JokePageComponent } from './joke-page/joke-page.component';
+
+
+
+const routes: Routes = [
+  { path: '',  component: MainPageComponent},
+  { path: 'selected', component: JokePageComponent  },
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule, RouterOutlet]
+})
+export class AppRoutingModule { }
--- src/app/mainPage/main-page-component.ts
+++ src/app/mainPage/main-page-component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../api.service'; 
 
 @Component({
-  selector: 'app-api-test',
+  selector: 'app-main-page',
   templateUrl: './main-page-component.html',
   styleUrls: ['./main-page-component.scss']
 })
 
-export class ApiTestComponent implements OnInit {
+export class MainPageComponent implements OnInit {
   jokeData: any[string]; 
   title!: string;
   imageUrl! : string; 
